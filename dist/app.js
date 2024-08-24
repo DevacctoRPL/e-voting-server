17bbadf842a5b4b08413fdbb58145e55feec48ea
Morpheus Zein <morpheus.zein@irawan.id>
2024-08-24 18:22:56 +0700

    adding datares route

--- dist/app.js
+++ dist/app.js
@@ -73,6 +73,55 @@ app.use(checkConnectionDB_1.default);
  *       example:
  *         NIU: '123456789'
  *         No_Pilihan: 2
+ *
+ *     DataLiveRes:
+ *       type: object
+ *       properties:
+ *         OSIS:
+ *           type: object
+ *           properties:
+ *             Pemilih_1:
+ *               type: number
+ *               description: Number of votes for the first OSIS candidate
+ *             Pemilih_2:
+ *               type: number
+ *               description: Number of votes for the second OSIS candidate
+ *             Pemilih_3:
+ *               type: number
+ *               description: Number of votes for the third OSIS candidate
+ *             Jumlah_Vote:
+ *               type: number
+ *               description: Total number of votes for OSIS
+ *         MPK:
+ *           type: object
+ *           properties:
+ *             Pemilih_1:
+ *               type: number
+ *               description: Number of votes for the first MPK candidate
+ *             Pemilih_2:
+ *               type: number
+ *               description: Number of votes for the second MPK candidate
+ *             Pemilih_3:
+ *               type: number
+ *               description: Number of votes for the third MPK candidate
+ *             Jumlah_Vote:
+ *               type: number
+ *               description: Total number of votes for MPK
+ *         Jumlah_User:
+ *           type: number
+ *           description: Total number of users
+ *       example:
+ *         OSIS:
+ *           Pemilih_1: 150
+ *           Pemilih_2: 200
+ *           Pemilih_3: 100
+ *           Jumlah_Vote: 450
+ *         MPK:
+ *           Pemilih_1: 120
+ *           Pemilih_2: 180
+ *           Pemilih_3: 90
+ *           Jumlah_Vote: 390
+ *         Jumlah_User: 500
  */
 // Routes
 /**
@@ -194,6 +243,61 @@ app.post('/UpdateMPK', async (req, res) => {
     }
     catch (error) {
         res.status(505).send("Internal server error");
+    }
+});
+/**
+ * @swagger
+ * /datares:
+ *   get:
+ *     summary: Get live voting results for OSIS and MPK
+ *     description: Retrieves the current voting counts for each OSIS and MPK candidate along with the total number of users.
+ *     responses:
+ *       200:
+ *         description: Successful retrieval of voting data
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/DataLiveRes'
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 err:
+ *                   type: string
+ */
+app.get('/datares', async (_, res) => {
+    try {
+        const [Pemilih_1_MPK, Pemilih_2_MPK, Pemilih_3_MPK, Pemilih_1_OSIS, Pemilih_2_OSIS, Pemilih_3_OSIS, Jumlah_User,] = await Promise.all([
+            prisma.user.count({ where: { Pilihan_MPK: 1 } }),
+            prisma.user.count({ where: { Pilihan_MPK: 2 } }),
+            prisma.user.count({ where: { Pilihan_MPK: 3 } }),
+            prisma.user.count({ where: { Pilihan_OSIS: 1 } }),
+            prisma.user.count({ where: { Pilihan_OSIS: 2 } }),
+            prisma.user.count({ where: { Pilihan_OSIS: 3 } }),
+            prisma.user.count(),
+        ]);
+        const ResDataObj = {
+            MPK: {
+                Pemilih_1: Pemilih_1_MPK,
+                Pemilih_2: Pemilih_2_MPK,
+                Pemilih_3: Pemilih_3_MPK,
+                Jumlah_Vote: Pemilih_1_MPK + Pemilih_2_MPK + Pemilih_3_MPK,
+            },
+            OSIS: {
+                Pemilih_1: Pemilih_1_OSIS,
+                Pemilih_2: Pemilih_2_OSIS,
+                Pemilih_3: Pemilih_3_OSIS,
+                Jumlah_Vote: Pemilih_1_OSIS + Pemilih_2_OSIS + Pemilih_3_OSIS,
+            },
+            Jumlah_User: Jumlah_User
+        };
+        res.status(200).json(ResDataObj);
+    }
+    catch (error) {
+        res.status(500).send({ message: "Error Occured.", err: error });
     }
 });
 exports.default = app;
--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -7,3 +7,19 @@ export type LoginUserReq = {
   NIU: string,
   password: number,
 }
+
+export type DataLiveRes = {
+  OSIS: {
+    Pemilih_1: number,
+    Pemilih_2: number,
+    Pemilih_3: number,
+    Jumlah_Vote: number,
+  },
+  MPK: {
+    Pemilih_1: number,
+    Pemilih_2: number,
+    Pemilih_3: number,
+    Jumlah_Vote: number,
+  },
+  Jumlah_User: Jumlah_User
+}
